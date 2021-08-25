58680982d3877b740ffcf95b1e401c7b24f43d41
Andre <katsuya.tsu@gmail.com>
2021-08-25 10:47:18 -0300

    Initial Commit

---
+++ MVVM pattern/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  MVVM pattern
+//
+//  Created by user on 25/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var tablewView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+
+}
+
