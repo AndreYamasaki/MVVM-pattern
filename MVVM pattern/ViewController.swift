ad25ca0ba342bb41eb84446b04bf148b3db9e121
Andre <katsuya.tsu@gmail.com>
2021-08-25 10:57:35 -0300

    added tableView and its methods

--- MVVM pattern/ViewController.swift
+++ MVVM pattern/ViewController.swift
@@ -7,16 +7,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet var tablewView: UITableView!
+    
+    var data = ["one", "two", "test"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+        tablewView.delegate = self
+        tablewView.dataSource = self
     }
-
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = data[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
 }
 
